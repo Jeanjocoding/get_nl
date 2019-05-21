08705241b99d111b1c60e7602fcebb17d49e6848
Theophile LUCILLE <tlucille@e2r10p4.42.fr>
2019-05-21 17:17:48 +0200

    TOUT EST BON

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tlucille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 18:09:09 by tlucille          #+#    #+#             */
-/*   Updated: 2019/05/14 14:31:17 by tlucille         ###   ########.fr       */
+/*   Updated: 2019/05/21 16:57:26 by tlucille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ char	*ft_strchr(const char *s, int c)
 {
 	unsigned char	c2;
 	unsigned int	i;
+	unsigned int	len;
 
 	i = 0;
+	len = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
 	c2 = c;
-	while (i <= ft_strlen(s))
+	while (i <= len)
 	{
 		if (s[i] == c2)
 			return ((char*)&s[i]);
--- marav_line.c
+++ marav_line.c
@@ -6,17 +6,34 @@
 /*   By: tlucille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 12:43:15 by tlucille          #+#    #+#             */
-/*   Updated: 2019/05/21 17:17:28 by tlucille         ###   ########.fr       */
+/*   Updated: 2019/05/21 16:36:09 by tlucille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
 #include "get_next_line.h"
 
-t_list		*bp_last_line(t_list **list, char **buf)
+int			ft_strlenc(char *s, int c)
+{
+	int		i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+
+t_list		*bp_last_line(t_list **list, char **buf, int len)
 {
 	(*list)->rest = NULL;
-	if (ft_strlen(*buf) > 0)
+	if (len > 0)
 	{
 		if (!((*list)->line = ft_strdup(*buf)))
 			return (NULL);
@@ -31,32 +48,30 @@ t_list		*bp_last_line(t_list **list, char **buf)
 	return (*list);
 }
 
-t_list		*buf_parser(char **buf)
+t_list		*buf_parser(char **buf, int len, int lenc)
 {
 	int		i;
 	t_list	*list;
+	char	line[lenc + 1];
+	char	rest[len - lenc];
 
 	i = 0;
 	list = NULL;
 	if (!(list = (t_list*)malloc(sizeof(t_list))))
 		return (NULL);
 	list->complet = 0;
-	if (ft_memchr(*buf, '\n', ft_strlen(*buf)) != NULL)
+	if (ft_strchr(*buf, '\n') != NULL)
 	{
-		if (!(list->line = ft_strextract(*buf, '\n', 0)))
-			return (NULL);
-		if (ft_strlen(list->line) < ft_strlen(*buf))
-		{
-			if (!(list->rest = ft_strextract(ft_strchr(*buf, '\n'), '\0', 1)))
-				return (NULL);
-		}
+		list->line = ft_strsub(*buf, 0, lenc);
+		if (lenc < len - 1)
+			list->rest = ft_strncpy(rest, &((*buf)[lenc]), (len + 1) - lenc);
 		else
 			list->rest = NULL;
 		ft_memdel((void**)buf);
 		return (list);
 	}
 	else
-		return (bp_last_line(&list, buf));
+		return (bp_last_line(&list, buf, len));
 }
 
 t_list		*line_finder(char **pline, int fd)
@@ -64,19 +79,19 @@ t_list		*line_finder(char **pline, int fd)
 	char	*join;
 	char	*temp;
 	char	buf[BUFF_SIZE + 1];
-	int		ret;
+	int		ret[2];
 
-	ret = 10;
+	ret[0] = 10;
 	join = NULL;
 	temp = ft_strchr(*pline, '\n');
-	while (temp == NULL && ret > 0)
+	while (temp == NULL && ret[0] > 0)
 	{
-		ret = read(fd, buf, BUFF_SIZE);
-		if (ret == -1)
+		ret[0] = read(fd, buf, BUFF_SIZE);
+		if (ret[0] == -1)
 			return (NULL);
-		else if (ret > 0)
+		else if (ret[0] > 0)
 		{
-			buf[ret] = '\0';
+			buf[ret[0]] = '\0';
 			if (!(join = ft_strjoin(*pline, buf)))
 				return (NULL);
 			ft_memdel((void**)pline);
@@ -84,13 +99,9 @@ t_list		*line_finder(char **pline, int fd)
 			temp = ft_strchr((char*)buf, '\n');
 		}
 	}
-	return (buf_parser(pline));
-}
-
-int			free_last(t_list **list)
-{
-	ft_memdel((void**)list);
-	return (0);
+	ret[0] = ft_strlen(*pline);
+	ret[1] = ft_strlenc(*pline, '\n');
+	return (buf_parser(pline, ret[0], ret[1]));
 }
 
 int			get_next_line(const int fd, char **line)
@@ -115,7 +126,10 @@ int			get_next_line(const int fd, char **line)
 	if (!(actual = line_finder(&prev[fd]->rest, fd)))
 		return (-1);
 	if (actual->complet == 1)
-		return (free_last(&actual));
+	{
+//		ft_memdel((void**)actual);
+		return (0);
+	}
 	*line = actual->line;
 	ft_memdel((void**)&prev[fd]);
 	prev[fd] = actual;
